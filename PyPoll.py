07e6b332e2abc93f86c3c0b14493bc09b7d7f5ea
Brooks Terrell <brooksterrell@brookss-mbp.attlocal.net>
2021-01-05 23:33:03 -0600

    Counted total votes in PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -5,28 +5,47 @@
 # 4) Percentage of votes per candidate
 # 5) The election winner (popular vote)
 
-#import datetime as dt
-#now = dt.datetime.now()
-#print(f"The time right now is {now}")
+# import datetime as dt
+# now = dt.datetime.now()
+# print(f"The time right now is {now}")
 
 import csv
 import os
-# Assign a variable for the file path tot the file
-# Use os.path.join("str","str") to make it work on either Mac or Windows
+# Assign a variable for the file path to the file
+# Use os.path.join("str","str") to make it work on either Mac or Windows OS
 file_to_load = os.path.join("Resources", "election_results.csv")
 
-# Create a file to save results to
+# Create a file (and file path) to save results to
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-with open(file_to_save, "w") as outfile:
-    # Write some data
-    outfile.write("Counties in the Election\n-------------------------\n")
-    outfile.write("Arapahoe\nDenver\nJefferson")
 
+#initialize the total vote counter
+total_votes = 0
 
-# Open the election results and read the file
-'''
+# Open the election results and read the file; use "with" so that it closes automatically
 with open(file_to_load) as election_data:
 
-    # To do: perform analysis
-    print(election_data)
+    # Read the file with reader function
+    file_reader = csv.reader(election_data)
+
+
+    # Read and print the Header Row
+    headers = next(file_reader)
+    print(headers)
+
+     # Print every row in the CSV file
+
+    for row in file_reader:
+        total_votes += 1
+    
+# Print total votes
+print(total_votes)
+
+
+'''
+# Open file using "with" so that it automatically closes
+with open(file_to_save) as outfile:
+    
+    # Code to read and analyze data here
+    outfile.write("Counties in the Election\n-------------------------\n")
+   outfile.write("Arapahoe\nDenver\nJefferson")
 '''
